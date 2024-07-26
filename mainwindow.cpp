585f6b0f3788e5a15d68dbe95e89cef5bb3394b9
VolodymirM <volodymir.moskalyuk@gmail.com>
2024-07-27 00:22:57 +0300

    "Import a products table (.xlsx)" button's functionality

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -4,6 +4,8 @@
 #include <QFileDialog>
 #include <QMessageBox>
 #include <QDebug>
+#include <QTableWidget>
+#include <vector>
 
 #include "xlsxdocument.h"
 #include "xlsxchartsheet.h"
@@ -12,7 +14,31 @@
 #include "xlsxrichstring.h"
 #include "xlsxworkbook.h"
 
+struct Product
+{
+    QString container;
+    QString pallet;
+    QString name;
+    QString customer;
+    Product(QVariant new_container, QVariant new_pallet, QVariant new_name) {
+        container = new_container.toString();
+        pallet = new_pallet.toString();
+        name = new_name.toString();
+    }
+};
+
+struct Customer
+{
+    QString name;
+    std::vector<int> product_quantity;
+    Customer(QVariant new_name) {name = new_name.toString();}
+};
+
 QString pr_filename;
+std::vector<Product> products;
+std::vector<Customer> customers;
+bool pr_imported = false, cl_imported = false, distributed = false;
+
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -30,9 +56,11 @@ void MainWindow::on_pushImport_pr_clicked()
 {
     pr_filename = QFileDialog::getOpenFileName(this,
         tr("Importing a product table"), "C://", "Excel file (*.xlsx)");
+
     if (pr_filename.isEmpty()) {
         qDebug() << "No file selected";
         return;
+
     }
     QXlsx::Document xlsx(pr_filename);
     if (!xlsx.load()) {
@@ -40,12 +68,15 @@ void MainWindow::on_pushImport_pr_clicked()
         qDebug() << "Failed to load the Excel file.";
         return;
     }
+
     QXlsx::Cell* test_suitable = xlsx.cellAt(1, 7); // Cell G2
     QXlsx::Cell* test_used = xlsx.cellAt(2, 6); // Cell F2
     QXlsx::Cell* test_empty = xlsx.cellAt(1, 1); // Cell A1
+
     bool isSuitableValid = test_suitable && test_suitable->readValue().isValid();
     bool isUsedValid = test_used && test_used->readValue().isValid();
     bool isEmptyValid = !(test_empty && test_empty->readValue().isValid());
+
     if (isUsedValid || isSuitableValid || isEmptyValid) {
         QMessageBox::critical(this, "Unable to import the table",
             "WARNING: It seems like you are trying to use unsuitable table, or your table was already used. "
@@ -61,7 +92,32 @@ void MainWindow::on_pushImport_pr_clicked()
     test_used = nullptr;
     test_empty = nullptr;
 
-    //Continue here
+    if (pr_imported)
+        products.clear();
+
+    unsigned count = 2;
+    while (xlsx.cellAt(count, 4) != nullptr) {
+        products.push_back(Product(xlsx.cellAt(count, 3)->readValue(), xlsx.cellAt(count, 4)->readValue(), xlsx.cellAt(count, 5)->readValue()));
+        qDebug() << products[count - 2].container << " " << products[count - 2].pallet << " " << products[count - 2].name;
+        ++count;
+    }
+
+    ui->tableProducts->setColumnCount(4);
+    ui->tableProducts->setRowCount(products.size());
+    ui->tableProducts->setHorizontalHeaderLabels(QString("EX-FILE - CONTAINER;PALLET #;Produce;Customer").split(";"));
+
+    for (count = 0; count < products.size(); ++count) {
+        ui->tableProducts->setItem(count, 0, new QTableWidgetItem(products[count].container));
+        ui->tableProducts->setItem(count, 1, new QTableWidgetItem(products[count].pallet));
+        ui->tableProducts->setItem(count, 2, new QTableWidgetItem(products[count].name));
+    }
+
+    ui->tableProducts->resizeColumnsToContents();
+    ui->tableProducts->resizeRowsToContents();
+    ui->tableProducts->horizontalHeader()->setSectionResizeMode(QHeaderView::ResizeToContents);
+    ui->tableProducts->verticalHeader()->setSectionResizeMode(QHeaderView::ResizeToContents);
+
+    pr_imported = true;
     return;
 }
 
